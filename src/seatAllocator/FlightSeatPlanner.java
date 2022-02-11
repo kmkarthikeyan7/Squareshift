1765168b9a5a1902f6b5c848fc4f2fec223a60ef
1591250 <Karthikeyan.Kannan1@sc.com>
2022-02-11 14:07:06 +0530

    Seating enhanced

--- src/seatAllocator/FlightSeatPlanner.java
+++ src/seatAllocator/FlightSeatPlanner.java
@@ -5,95 +5,101 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class FlightSeatPlanner {
-	List<LinkedList<SeatDetail>> seats;
+    private List<LinkedList<SeatDetail>> seats;
 
-	List<LinkedList<SeatDetail>> getSeats() {
-		return seats;
-	}
+    public List<LinkedList<SeatDetail>> getSeats() {
+        return seats;
+    }
 
-	public void initFlightSeats(int n, int[][] seatMatrix) {
-		seats = new ArrayList<>(n);
+    public void initFlightSeats(int n, int[][] seatMatrix) {
+        seats = new ArrayList<>(n);
 
-		for (int i = 0; i < seatMatrix[0][1]; i++) {
-			LinkedList<SeatDetail> list = new LinkedList<>();
-			for (int j = 0; j < seatMatrix[0][0]; j++) 
-			{
-				SeatDetail seatDetail = new SeatDetail();
-				seatDetail.setSeatInFlightPos(i, j);
-				if (j == 0) {
-					seatDetail.setSeatType(SeatType.WINDOW);
-				} else if (j == seatMatrix[0][0] - 1) {
-					seatDetail.setSeatType(SeatType.AISLE);
-				} else {
-					seatDetail.setSeatType(SeatType.CENTER);
-				}
+        for(int i = 0; i < seatMatrix[0][1]; i++) {
+            LinkedList<SeatDetail> list = new LinkedList<>();
+            for(int j = 0; j < seatMatrix[0][0]; j++) {
+                SeatDetail seatDetail = new SeatDetail();
+                seatDetail.setSeatInFlightPos(i, j);
+                if(j == 0) {
+                    seatDetail.setSeatType(SeatType.WINDOW);
+                } else if(j == seatMatrix[0][0] - 1) {
+                    seatDetail.setSeatType(SeatType.AISLE);
+                } else {
+                    seatDetail.setSeatType(SeatType.CENTER);
+                }
 
-				list.add(seatDetail);
-			}
-			seats.add(list);
-		}
+                list.add(seatDetail);
+            }
+            seats.add(list);
+        }
 
-		int seatColOffset = seatMatrix[0][0];
-		for (int inp = 1; inp < seatMatrix.length - 1; inp++) {
-			for (int i = 0; i < seatMatrix[inp][1]; i++) {
-				int seatColStartPos = seatMatrix[0][0];
-				for (int j = 0; j < seatMatrix[inp][0]; j++) {
-					SeatDetail seatDetail = new SeatDetail();
-					seatDetail.setSeatInFlightPos(i, seatColOffset + j);
-					if (j > 0 && j < seatMatrix[inp][0] - 1) {
-						seatDetail.setSeatType(SeatType.CENTER);
-					} else {
-						seatDetail.setSeatType(SeatType.AISLE);
-					}
+        int seatColOffset = seatMatrix[0][0];
+        for(int inp = 1; inp < seatMatrix.length - 1; inp++) {
+            for(int i = 0; i < seatMatrix[inp][1]; i++) {
+                for(int j = 0; j < seatMatrix[inp][0]; j++) {
+                    SeatDetail seatDetail = new SeatDetail();
+                    seatDetail.setSeatInFlightPos(i, seatColOffset + j);
+                    if(j > 0 && j < seatMatrix[inp][0] - 1) {
+                        seatDetail.setSeatType(SeatType.CENTER);
+                    } else {
+                        seatDetail.setSeatType(SeatType.AISLE);
+                    }
 
-					LinkedList<SeatDetail> list;
-					try {
-						list = seats.get(i);
-					} catch (IndexOutOfBoundsException e) {
-						list = new LinkedList<>();
-						seats.add(list);
-					}
-					list.add(seatDetail);
-				}
-			}
-			seatColOffset += seatMatrix[inp][0];
-		}
+                    LinkedList<SeatDetail> list;
+                    try {
+                        list = seats.get(i);
+                    } catch (IndexOutOfBoundsException e) {
+                        list = new LinkedList<>();
+                        seats.add(list);
+                    }
+                    list.add(seatDetail);
+                }
+            }
+            seatColOffset += seatMatrix[inp][0];
+        }
 
-		for (int i = 0; i < seatMatrix[seatMatrix.length - 1][1]; i++) {
-			for (int j = 0; j < seatMatrix[seatMatrix.length - 1][0]; j++) {
-				SeatDetail seatDetail = new SeatDetail();
-				seatDetail.setSeatInFlightPos(i, seatColOffset + j);
-				if (j == 0) {
-					seatDetail.setSeatType(SeatType.AISLE);
-				} else if (j == seatMatrix[0][0] - 1) {
-					seatDetail.setSeatType(SeatType.WINDOW);
-				} else {
-					seatDetail.setSeatType(SeatType.CENTER);
-				}
+        for(int i = 0; i < seatMatrix[seatMatrix.length - 1][1]; i++) {
+            for(int j = 0; j < seatMatrix[seatMatrix.length - 1][0]; j++) {
+                SeatDetail seatDetail = new SeatDetail();
+                seatDetail.setSeatInFlightPos(i, seatColOffset + j);
+                if(j == 0) {
+                    seatDetail.setSeatType(SeatType.AISLE);
+                } else if(j == seatMatrix[seatMatrix.length - 1][0] - 1) {
+                    seatDetail.setSeatType(SeatType.WINDOW);
+                } else {
+                    seatDetail.setSeatType(SeatType.CENTER);
+                }
 
-				LinkedList<SeatDetail> list;
-				try {
-					list = seats.get(i);
-				} catch (IndexOutOfBoundsException e) {
-					list = new LinkedList<>();
-					seats.add(list);
-				}
-				list.add(seatDetail);
-			}
-		}
-	}
+                LinkedList<SeatDetail> list;
+                try {
+                    list = seats.get(i);
+                } catch (IndexOutOfBoundsException e) {
+                    list = new LinkedList<>();
+                    seats.add(list);
+                }
+                list.add(seatDetail);
+            }
+        }
+    }
 
-	public void printSeats() {
-		for (LinkedList<SeatDetail> seatDetails : seats) {
-			System.out.println();
-			for (SeatDetail seatDetail : seatDetails) {
+    public void printSeats() {
+        for (LinkedList<SeatDetail> seatDetails: seats) {
+            System.out.println();
+            int startSeatPositionInFlight = 0;
+            for (SeatDetail seatDetail : seatDetails) {
+                if(seatDetail.j != startSeatPositionInFlight) {
+                    for (int k = startSeatPositionInFlight; k < seatDetail.j; k++) {
+                        System.out.printf("%-18s", "");
+                    }
+                }
 				String passnum=Integer.toString(seatDetail.getPassNumber());
 				if(passnum.equals("-1")) {
 					passnum ="Empty";
 				}
-				
-				System.out.printf("%-18s", (seatDetail.getSeatType() + ":" + passnum));
-			}
-		}
-	}
+                startSeatPositionInFlight = seatDetail.j + 1;
+                System.out.printf("%-18s", (seatDetail.getSeatType() + ":" + passnum));
+            }
+        }
+    }
+
+
 }
--- src/seatAllocator/PassengerSeatAssigner.java
+++ src/seatAllocator/PassengerSeatAssigner.java
@@ -4,43 +4,44 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PassengerSeatAssigner {
+    public void assignSeats(List<LinkedList<SeatDetail>> seats,
+                            int aisleStartPos,
+                            int windowStartPos,
+                            int centerStartPos,
+                            int noOfPass) {
 
-	public void assignSeats(List<LinkedList<SeatDetail>> seats, int aisleStartPos, int windowStartPos,
-			int centerStartPos, int noOfPass) {
+        int aislePassNumber = aisleStartPos;
+        int windowPassNumber = windowStartPos;
+        int centerPassNumber = centerStartPos;
+        int totalNumberPassenger = noOfPass;
 
-		int aislePassNumber = aisleStartPos;
-		int windowPassNumber = windowStartPos;
-		int centerPassNumber = centerStartPos;
-		int totalNumberPassenger = noOfPass;
-
-		for (LinkedList<SeatDetail> seatDetails : seats) {
-			for (SeatDetail seatDetail : seatDetails) {
-				switch (seatDetail.getSeatType()) {
-				case AISLE:
-					if (aislePassNumber < windowStartPos) {
-						seatDetail.setPassNumber(aislePassNumber++);
-					}
-					break;
-				case WINDOW:
-					if (windowPassNumber < centerStartPos) {
-						seatDetail.setPassNumber(windowPassNumber++);
-					}
-					break;
-				case CENTER:
-					if (centerPassNumber <= totalNumberPassenger) {
-						seatDetail.setPassNumber(centerPassNumber++);
-					}
-					break;
-				default:
-					throw new UnsupportedOperationException();
-				}
-			}
-			noOfPass--;
-			if (noOfPass == 0) {
-				System.out.println("All seats filled for waiting passenger");
-				break;
-			}
-		}
-	}
-
+        for (LinkedList<SeatDetail> seatDetails: seats) {
+            for (SeatDetail seatDetail : seatDetails) {
+                switch (seatDetail.getSeatType()) {
+                    case AISLE:
+                        if(aislePassNumber < windowStartPos) {
+                            seatDetail.setPassNumber(aislePassNumber++);
+                        }
+                        break;
+                    case WINDOW:
+                        if(windowPassNumber < centerStartPos) {
+                            seatDetail.setPassNumber(windowPassNumber++);
+                        }
+                        break;
+                    case CENTER:
+                        if(centerPassNumber <= totalNumberPassenger) {
+                            seatDetail.setPassNumber(centerPassNumber++);
+                        }
+                        break;
+                    default:
+                        throw new UnsupportedOperationException();
+                }
+            }
+            noOfPass--;
+            if(noOfPass == 0) {
+                System.out.println("All seats filled for waiting passenger");
+                break;
+            };
+        }
+    }
 }
